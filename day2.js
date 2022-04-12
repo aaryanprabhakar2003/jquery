e9f9fa829db8e02dfe5cf31c521aa4d52005952e
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-04-12 11:33:01 +0530

    updated file of day2

--- day2.js
+++ day2.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){ 
+    // doubleclick event
     $('p').dblclick(function(){
-        console.log("you doubled clicked on p",this);
+       // console.log("you doubled clicked on p",this);
+       //alert("you doubled clicked on p",this);
     });
-    $('p').mouseenter(function(){ 
-        console.log("you entered ",this);
+    // mouse enter event
+    $('p').mouseenter(function(){   
+       // console.log("you entered ",this);
+       //alert("you entered",this);
+        // mouse leave event
     });
-    $('#btn1').click(function(){
-        $('p').hide();
-    })
-
+    $('p').mouseleave(function(){
+        //console.log("you left ",this);
+        //alert("you left ",this);
+    });
+    // mouse hover event
+    $('p').hover(function(){
+        //alert("mouse hover",this);
+    });
 });
