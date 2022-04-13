d6096b358ad742dabaf4b35501a04e788a5b35ea
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-04-13 12:21:08 +0530

    day3 jquery

---
+++ day3.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+// hide content in 3 seconds
+
+/*$("#learning").hide(3000,function(){
+    alert("hide completed");
+});*/
+
+// show content in 5 seconds after hide is done
+/*$("#learning").show(5000,function(){
+    alert("show completed");
+});*/
+
+// hide content when button is clicked
+/*$("#btn1").click(function(){
+ $("#learning").hide(3000,function(){
+});*/
+
+// hide all content on page
+/*$("#btn2").click(function(){
+    $('*').hide(1000)
+});*/
+
+// fade out and fade in  the content when button is clicked
+
+/*$("#btn2").click(function(){
+    $("#learning").fadeOut(1000);
+    $("#learning").fadeIn(1000);
+});*/
+
+// silde up and slide down the content when button is clicked
+
+/*$("#btn2").click(function(){
+    $("#learning").slideUp(1000)
+    $("#learning").slideDown(2000)
+});*/
+
+//hide content when key pressed in the textbox
+
+/*$("#text1").keypress(function(){
+    alert("key pressed")
+});*/
+
+});
