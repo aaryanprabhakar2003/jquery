25f2b8bbb414bdd3db42e20bf2313568b053a402
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-04-18 10:41:44 +0530

    updated day4.js

--- day4.js
+++ day4.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     $("#btn").click(function(){
-        $('p').append("  I AM JQUERY"); // append text at end 
-        alert("added text at end")
+        /*$('p').append("  I AM JQUERY"); // append text at end 
+        alert("added text at end")*/
+
         /*$('p').prepend("i am jquery"); // prepend text at start
-        alert("added text at start")
-        $('p').after(" i am "); // add text to new line
-        alert("added text after line")
-        $('p').before(" jquery");   // add text before new line
+        alert("added text at start")*/
+
+        /*$('p').after(" i am "); // add text to new line
+        alert("added text after line")*/
+        
+        /*$('p').before(" jquery");   // add text before new line
         alert("added text before line")*/
     });
 });
